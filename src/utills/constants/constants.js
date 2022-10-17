7c8dae4a1fcaf07295641eeed4b45735a3999423
zohaib-SoftwareEngineer <zohaibsaddiqui@gmail.com>
2022-10-17 17:26:27 +0500

    First Commit

---
+++ src/utills/constants/constants.js
@@ -0,0 +1,9 @@
+import abiPFP from './abiPFP.json';
+import abiICO from './abiICO.json';
+export const pfpContractAddress = '0x0921E508fd9EEfe54e03424b269CafC89b379593';
+export const wbtcContractAddress = '0xeb2c88AE22dBc1a944adc555F272d1C435F62E3B';
+export const usdtContractAddress = '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd';
+export const icoContractAddress = '0x9F6a8B51dB5A4E95e55737DB817951b5363d424D';
+
+export const pfpAbi = abiPFP;
+export const icoAbi = abiICO;
