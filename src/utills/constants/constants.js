7446eb3ec844b89c2cc59c23e54bcd141d2f85d7
zohaib-SoftwareEngineer <zohaibsaddiqui@gmail.com>
2022-11-23 03:15:21 +0500

    contract update

--- src/utills/constants/constants.js
+++ src/utills/constants/constants.js
@@ -3,7 +3,7 @@ import abiICO from './abiICO.json';
 export const pfpContractAddress = '0x0921E508fd9EEfe54e03424b269CafC89b379593';
 export const wbtcContractAddress = '0xeb2c88AE22dBc1a944adc555F272d1C435F62E3B';
 export const usdtContractAddress = '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd';
-export const icoContractAddress = '0x9F6a8B51dB5A4E95e55737DB817951b5363d424D';
+export const icoContractAddress = '0x6EF8B53b539d7eB9Dbbc0CF07e5ee2DE81b13D05';
 
 export const pfpAbi = abiPFP;
 export const icoAbi = abiICO;
